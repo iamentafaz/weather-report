5f9c8a1a662979c401177c3507763a24b503ab51
Mir entafaz Ali <77488447+iamentafaz@users.noreply.github.com>
2023-07-30 22:37:26 +0530

    Clean up and little bit of improvement

--- scripts.js
+++ scripts.js
@@ -10,8 +10,8 @@ function getLocation() {
 }
 
 function getWeatherInfo(position) {
-  let long = position.coords.longitude;
-  let lati = position.coords.latitude;
+  const long = position.coords.longitude;
+  const lati = position.coords.latitude;
   const xhttp = new XMLHttpRequest();
   xhttp.open("GET", "https://fcc-weather-api.glitch.me/api/current?lat="+lati+"&lon="+long+"", true);
   xhttp.send();
@@ -23,13 +23,12 @@ function getWeatherInfo(position) {
 }
 
 function loadDetailsOfThePage(data) {
-    let details = JSON.parse(data);
-    console.log(details);
-    let iconSrc = details.weather[0].icon;
-    let name = details.name;
-    let temp = details.main['temp'];
-    let description = details.weather[0].description;
-    let windSpeed = details.wind['speed'];
+    const details = JSON.parse(data);
+    const iconSrc = details.weather[0].icon;
+    const name = details.name;
+    const temp = details.main['temp'];
+    const description = details.weather[0].description;
+    const windSpeed = details.wind['speed'];
     document.getElementById('weatherIcon').innerHTML = "<img src="+iconSrc+">";
     document.getElementById("temp").innerHTML = parseInt(temp);
     document.querySelector('.placeHolder .place').innerHTML = name;
@@ -39,14 +38,14 @@ function loadDetailsOfThePage(data) {
 document.getElementById('far').addEventListener('click', ()=> {
     document.getElementsByClassName('tempChange')[0].classList.toggle('active');
     document.getElementsByClassName('tempChange')[1].classList.toggle('active');
-    let c = document.getElementById("temp").innerHTML;
-    let f = Math.floor((9*c + 160)/5);
+    const c = document.getElementById("temp").innerHTML;
+    const f = Math.floor((9*c + 160)/5);
     document.getElementById("temp").innerHTML = f;
 });
 document.getElementById('cen').addEventListener('click', ()=> {
     document.getElementsByClassName('tempChange')[0].classList.toggle('active');
     document.getElementsByClassName('tempChange')[1].classList.toggle('active');
-    let f = document.getElementById("temp").innerHTML;
-    let c = Math.floor((5*f - 160)/9);
+    const f = document.getElementById("temp").innerHTML;
+    const c = Math.floor((5*f - 160)/9);
     document.getElementById("temp").innerHTML = c;
 });
